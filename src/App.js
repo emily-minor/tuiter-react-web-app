969cabbe5027e8e256714d1f138caba43011600b
emilymlynn <minor.e@husky.neu.edu>
2022-10-21 10:49:55 -0400

    2.6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
 
 function App() {
     return (
         <div className="container">
             <HelloWorld/>
             <Labs/>
+            <Tuiter/>
         </div>
     );
 }
