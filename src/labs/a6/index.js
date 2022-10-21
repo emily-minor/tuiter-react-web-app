152234e33616a72833a62ea3822e307882f924fc
emilymlynn <minor.e@husky.neu.edu>
2022-10-21 10:55:42 -0400

    2.9

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,14 +1,9 @@
-import {Link}
-    from "react-router-dom";
-
+import Nav from "../../nav";
 function Assignment6() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
             <h1>Assignment 6</h1>
         </div>
-    )
+    );
 }
 export default Assignment6;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,8 @@
-import {Link}
-    from "react-router-dom";
-
+import Nav from "../nav";
 function Tuiter() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
+            <Nav/>
             <h1>Tuiter</h1>
         </div>
     )
