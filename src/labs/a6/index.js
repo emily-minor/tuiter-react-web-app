770ca796b7b6484d6fe053d4738e1b6796914b96
emilymlynn <minor.e@husky.neu.edu>
2022-10-21 11:02:23 -0400

    2.10

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,9 +1,12 @@
-import Nav from "../../nav";
+import Classes from "./classes";
+
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
+            <Classes/>
         </div>
     );
 }
 export default Assignment6;
+
