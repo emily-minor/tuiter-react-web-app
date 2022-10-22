7f80db1174cb9a1d89d01e0bf079b26e6ed3d0a1
emilymlynn <minor.e@husky.neu.edu>
2022-10-22 12:26:43 -0400

    3.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
+import Nav from "./nav"
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
@@ -10,11 +11,13 @@ function App() {
             <div className="container">
                 <Routes>
                     <Route index
-                           element={<Labs/>}/>
-                    <Route path="/hello"
-                           element={<HelloWorld/>}/>
+                           element={<Nav/>}/>
                     <Route path="/tuiter"
                            element={<Tuiter/>}/>
+                    <Route path="/hello"
+                           element={<HelloWorld/>}/>
+                    <Route path="/labs"
+                           element={<Labs/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,22 @@
 import Nav from "../nav";
+import NavigationSidebar
+    from "./navigation-sidebar";
+import WhoToFollowListItem
+    from "./who-to-follow-list/index";
+import PostSummaryItem from "./post-summary-list/post-summary-item";
+
 function Tuiter() {
     return (
         <div>
             <Nav/>
+            <NavigationSidebar active="home"/>
+            <WhoToFollowListItem/>
+            {/*<PostSummaryItem/>*/}
+
+
             <h1>Tuiter</h1>
         </div>
     )
 }
+
 export default Tuiter
